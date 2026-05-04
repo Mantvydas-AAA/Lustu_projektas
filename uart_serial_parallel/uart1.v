15e4f6e79cd5d7dc1faf64429b2635cc5448e242
DanieliusBert <danber2@ktu.lt>
2026-05-04 18:56:17 +0300

    Update uart1.v

--- uart_serial_parallel/uart1.v
+++ uart_serial_parallel/uart1.v
@@ -10,9 +10,9 @@ module uart_8bitserial(
 
   input wire reset,
 
-  output reg [7:0] parallel_out  
+  output reg [7:0] parallel_out,  
 
-
+  output reg [3:0] counter
 
 );
 
@@ -24,25 +24,37 @@ module uart_8bitserial(
 
    
 
-     reg [7:0] shift_register;
-
+    reg [7:0] shift_register;
+   
+    reg stop;
 
 
 always @(posedge clk or posedge reset) begin
 
 
 
-        if (reset) begin
+        if (reset || stop) begin
 
+            counter <= 8'd0;
             shift_register <= 8'b0;
+            parallel_out <= 8'b0;   
+            stop <= 0;
 
-            parallel_out <= 8'b0;                    
+        end else if (counter == 4'd0 || counter ==4'd8 ) begin
 
+            counter <= counter + 1;                 
+      
         end else if (shift) begin
 
+          counter <= counter +1 ;
           shift_register <= {shift_register[6:0], serial_in};
+          parallel_out <= shift_register;
 
-            parallel_out <= shift_register;
+
+          
+          end if (counter == 4'd9) begin
+
+                      stop <= 1;
 
         end
 
